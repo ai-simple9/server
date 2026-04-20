2e7069ec022b5f0384d5c36826c35be777abfa84
ai-simple9 <nursultankadyrbekov27@gmail.com>
2026-04-20 12:22:50 +0600

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify, request
 import json
 import hashlib
 from datetime import date
+import os
 
 app = Flask(__name__)
 
-# ====================== БАЗА ПОЛЬЗОВАТЕЛЕЙ ======================
 users = {
     "admin": {
         "password": hashlib.sha256("az1x@d0s".encode()).hexdigest(),
-        "max_attacks": 100000,
+        "max_attacks": 9999,
         "attacks_today": 0,
         "last_date": str(date.today())
     },
@@ -23,7 +23,6 @@ users = {
 
 @app.route('/api/users', methods=['GET'])
 def get_users():
-    # Обновляем дату при запросе
     today = str(date.today())
     for u in users:
         if users[u]["last_date"] != today:
@@ -35,14 +34,14 @@ def get_users():
 def update_users():
     global users
     try:
-        new_users = request.get_json()
-        if new_users:
-            users = new_users
-            return jsonify({"status": "ok", "message": "База обновлена"})
+        new_data = request.get_json()
+        if new_data:
+            users = new_data
+            return jsonify({"status": "ok"})
         return jsonify({"status": "error"}), 400
     except:
         return jsonify({"status": "error"}), 500
 
 if __name__ == '__main__':
-    print("Сервер запущен → http://127.0.0.1:5000/api/users")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
